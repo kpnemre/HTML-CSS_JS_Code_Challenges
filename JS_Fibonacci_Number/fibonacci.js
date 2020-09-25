65ad918fa802d8c2ba8804b9d4ed6a1e404e82ff
kpnemre <kpnemre51@gmail.com>
2020-09-25 16:28:39 +0300

    Fibonacci Numbers with Javascript

---
+++ JS_Fibonacci_Number/fibonacci.js
@@ -0,0 +1,10 @@
+     
+        document.getElementById("btn_1").addEventListener("click", fibonacci)
+        function fibonacci() {
+            const n = document.getElementById("inp_1").value;
+            const arr= [1,1];
+            for (let i=2; i<n+1;i++){
+                arr.push(arr[i-2] + arr[i-1]);
+            }
+            document.getElementById("res").innerHTML = arr[n];   
+        }
