df0e7c15b66e682427bf6d80035510e42e230b24
kpnemre <kpnemre51@gmail.com>
2020-10-01 10:51:48 +0300

    Updated and completed

--- JS_Change_Color/index.js
+++ JS_Change_Color/index.js
@@ -1,8 +1,55 @@
+const colors = ["red", "blue","gray", "purple"];
 
 const buttonElement= document.querySelector("#randomColorButton");
+const buttonSet = document.querySelector("#setColorButton");
 
 buttonElement.addEventListener("click", changeColors);
 // const container = document.querySelector(".container");
+buttonSet.addEventListener("click", addColor);
+
+
+function changeColors (){
+
+    // document.querySelector("body").style.backgroundColor= getRandomColor();
+    // document.querySelector("#colorName").textContent= getRandomColor();
+// 
+
+    const randomIndex = Math.floor(Math.random()* colors.length);
+
+
+    document.querySelector("#colorName").textContent= colors[randomIndex];
+    document.querySelector("body").style.backgroundColor= colors[randomIndex];
+
+    
+};
+
+function addColor(){
+    const userInput = document.querySelector("#addColor");
+
+    // array içinde yoksa index i -1 olur.
+    if (userInput.value ==""){
+        alert("Please enter a color");
+                userInput.value ="";
+        userInput.focus();
+
+    }
+    
+    // indexof sadece flat array lerde geçerlidir. ["red", "blue",....]
+    // Object arrayler de findIndex geçerlidir. 
+    else if(colors.indexOf(userInput.value)===-1){
+        colors.push(userInput.value);
+        document.querySelector("body").style.backgroundColor= userInput.value ;
+        userInput.value ="";
+        userInput.focus();
+    }
+    else{
+        alert("Color is already exist");
+    }
+
+    
+    console.log(userInput.value);
+    console.log(colors);
+}
 
 
 function getRandomColor() {
@@ -14,18 +61,6 @@ function getRandomColor() {
     return color;
     }
 
-function changeColors (){
-
-    document.querySelector("body").style.backgroundColor= getRandomColor();
-    document.querySelector("#colorName").textContent= getRandomColor();
-
-    
-};
-
-
-// const colors = ["red", "blue","gray", "purple"];
-
-
 // function changeColors (){
 //     const randomIndex = Math.floor(Math.random()* colors.length);
 //     // array uzunluguna kadar olan sayıları random al
@@ -34,3 +69,23 @@ function changeColors (){
 //     document.querySelector("body").style.backgroundColor= getRandomColor();
     
 // };
+
+
+// ----------------Array Object array olduğunda-----
+
+  // indexOf SADECE flat array (["red", "blue"] / [1, 2, 3]) geçerlidir.
+  // findIndex object arraylerde geçerlidir.
+//   const searchedColorIndex = colors.findIndex(
+//     (myColor) => myColor.name === colorObject.name
+//   );
+//   if (searchedColorIndex === -1) {
+//     document.querySelector("body").style.backgroundColor = colorObject.name;
+//     document.querySelector("#colorName").textContent = colorObject.name;
+//     document.querySelector("#colorDesc").textContent = colorObject.motto;
+
+//     colors.push(colorObject);
+//     userInput.value = "";
+//     userInput.focus();
+//   } else {
+//     alert("Renk zaten mevcut!");
+//   }
--- JS_Change_Color/readme.txt
+++ JS_Change_Color/readme.txt
@@ -3,3 +3,9 @@
 In this challange, there is a button on the page. When you push the 
 the button, page background color changes randomly.
 there are two function. one of them producing random color. Other is changing color function.
+
+#Change: getting color is not randomly.
+steps:
+create a color array and get colors in this array randomly.
+if user enter a new color, background is setted that color and same 
+color added the color array.
