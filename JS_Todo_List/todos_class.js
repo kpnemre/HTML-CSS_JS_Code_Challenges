5d9b9eeb47fc67f5efedf6a68cb68a8a21769415
kpnemre <kpnemre51@gmail.com>
2020-10-05 23:53:28 +0300

    Create Todo_List

---
+++ JS_Todo_List/todos_class.js
@@ -0,0 +1,100 @@
+/*
+ * 1) TODO'ya tekrar basılınca, todo durumu eski haline gelsin
+ *    İpucu (Tek bir satırda değişiklik yapılacak)
+ *
+ * 2) Todo silme operasyonu
+ **/
+
+const todoList = [];
+const todoListElement = document.querySelector('#myUL');
+document.querySelector('#todo_button').addEventListener('click', addTodo);
+
+// console.log(todoListElement);
+function addTodo () {
+  const todoText = document.querySelector('#myInput').value;
+
+    const todoObject = {
+      // id: todoList.length * 5,
+      id: todoList.length,
+      todoText: todoText,
+      isDone: false,
+
+};
+
+todoList.push(todoObject);
+// console.log(todoList);
+displayTodos();
+
+}
+
+function doneTodo (todoId){
+  //indexof object lerde kullanılmaz
+  const selectedTodoIndex = todoList.findIndex(myTodo => myTodo.id==todoId);
+  todoList[selectedTodoIndex].isDone = true;
+
+displayTodos ();
+}
+
+function displayTodos(){
+
+  document.querySelector('#myInput').value= "";
+
+  todoListElement.innerHTML = "";
+  todoList.forEach(item=> {
+
+     const listElement = document.createElement("li");
+     listElement.innerHTML= item.todoText;
+     listElement.setAttribute('data-id', item.id);
+
+     if (item.isDone) {
+      listElement.classList.add('checked'); 
+     }
+     listElement.addEventListener('click',function(e){
+        const selectedId = e.target.getAttribute("data-id");
+        doneTodo (selectedId);
+     });
+
+     todoListElement.appendChild(listElement);
+ 
+  });
+}
+
+
+
+
+
+
+
+//   display() {
+//     this.todoListElement.innerHTML = "";
+
+//     todoList.forEach((item) => {
+//       const listElement = document.createElement("li");
+
+//       listElement.innerText = item.todoText;
+//       listElement.setAttribute("data-id", item.id);
+
+//       if (item.isDone) {
+//         listElement.classList.add("checked");
+//       }
+
+//       listElement.addEventListener("click", function (e) {
+//         const selectedId = e.target.getAttribute("data-id");
+//         myTodoList.done(selectedId);
+//       });
+
+//       this.todoListElement.appendChild(listElement);
+//     });
+//   }
+// }
+
+// const listSection = document.querySelector("#myUL");
+// const secondList = document.querySelector("#severekAyrilanlar");
+
+// const myTodoList = new TodoList(listSection);
+
+// document.querySelector("#todo_button").addEventListener("click", function () {
+//   const todoText = document.querySelector("#myInput").value;
+
+//   myTodoList.add(todoText);
+// });
