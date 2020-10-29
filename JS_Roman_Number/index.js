0f5a3fb192bcff3b7b3682ff97269548475624d7
kpnemre <kpnemre51@gmail.com>
2020-10-29 21:03:26 +0300

    Roman<=> Number  Convert

---
+++ JS_Roman_Number/index.js
@@ -0,0 +1,37 @@
+
+// ----Roman to Number Convert
+
+let userinput = document.getElementById('userinput');
+const useroutput = document.getElementById('useroutput');
+
+document.querySelector('#button').addEventListener('click', RomanConvert);
+
+
+
+function RomanConvert () {
+
+
+
+    console.log(output);
+  useroutput.innerHTML =  output;
+
+
+}
+// -----------------------------------------------------------------------
+
+// ---- Number to Roman  Convert
+ 
+let userinput2 = document.getElementById('userinput2');
+const useroutput = document.getElementById('useroutput2');
+
+document.querySelector('#button2').addEventListener('click', NumberConvert);
+
+function NumberConvert () {
+
+    
+    
+        console.log(output);
+      useroutput2.innerHTML =  output;
+    
+    
+    }
