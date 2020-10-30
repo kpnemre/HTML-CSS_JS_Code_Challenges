425aa92ac75abfccd9b3b8d6e9fffd3cd1d80da4
kpnemre <kpnemre51@gmail.com>
2020-10-30 16:43:25 +0300

    completed

--- JS_Roman_Number/index.js
+++ JS_Roman_Number/index.js
@@ -8,20 +8,35 @@ document.querySelector('#button').addEventListener('click', RomanConvert);
 
 
 
-let roman = '';
+
+
+
 function RomanConvert() {
+
+  let convertNumber= 0;
+  useroutput.innerHTML ='';
 
     num = userinput.value.toUpperCase().split('');
 
     let lookup = {M:1000,CM:900,D:500,CD:400,C:100,XC:90,L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1};
   
-    let i;
+    let i= 0;
+    let j;
     
-    for (i in num) {
+    
+    for (i=0;i<num.length; i++) {
+      for (j in lookup){
+          if (j === num[i]){
+            convertNumber += parseInt(lookup[j])
+          }
+      }
+      
         
     }
+    console.log(convertNumber);
+    useroutput.innerHTML = convertNumber;
     
-    console.log(num)
+    
     // for ( i in lookup ) {
     //   while ( num >= lookup[i] ) {
     //     roman += i;
@@ -30,9 +45,8 @@ function RomanConvert() {
     // }
     // console.log(roman)
     // useroutput2.innerHTML = roman;
-
-
   }
+ 
 
 
 // -----------------------------------------------------------------------
@@ -44,15 +58,17 @@ const useroutput2 = document.getElementById('useroutput2');
 
 document.querySelector('#button2').addEventListener('click', NumberConvert);
 
-let number = '';
-function NumberConvert() {
 
+function NumberConvert() {
+  let number = '';
+  useroutput2.innerHTML = '';
     num = userinput2.value;
     var lookup = {M:1000,CM:900,D:500,CD:400,C:100,XC:90,L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1};
   
     let i;
     
     for ( i in lookup ) {
+
       while ( num >= lookup[i] ) {
         number += i;
         num -= lookup[i];
